178e1a80a6caa321aa9030f1485076e5053ac01b
Mo Cuishle <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2019-04-12 17:16:26 +0530

    Create learningGraph.py

---
+++ learningGraph.py
@@ -0,0 +1,14 @@
+n, m, k = map(int, input().split())
+val = list(map(int, input().split()))
+adj = [[] for _ in range(n)]
+for _ in range(m):
+	x, y = map(int, input().split())
+	adj[x-1].append((val[y-1], y))
+	adj[y-1].append((val[x-1], x))
+for i in range(n):
+	adj[i].sort()
+for i in range(n):
+	if len(adj[i]) < k:
+		print(-1)
+	else:
+		print(adj[i][len(adj[i])-k][1])
