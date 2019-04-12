42b125d4a6f69ad3844093e55478cd2231a02483
Mo Cuishle <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2019-04-12 15:56:23 +0530

    Create monkAtTheGraphFactory.cpp

---
+++ monkAtTheGraphFactory.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, a, ans = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a;
+        ans += a;
+    }
+    (ans / 2 == n - 1) ? cout << "Yes\n" : cout << "No\n";
+}
