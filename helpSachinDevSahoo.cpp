fe7044fbd65c0cfdd47eb841b0c15847b8e5d1c6
Mo Cuishle <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2019-04-12 12:32:46 +0530

    Create helpSachinDevSahoo.cpp

---
+++ helpSachinDevSahoo.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        string str;
+        cin >> str;
+        int n, times = 0;
+        cin >> n;
+        for (int i = 0; i <= str.size()-n; i++) {
+            if (str[i] == '0') {
+                for (int j = i; j < i+n; j++)
+                    str[j] ^= '0' ^ '1';
+                times++;
+            }
+        }
+        bool flag = true;
+        for (int i = 0; i < str.size(); i++)
+            if (str[i] == '0') {
+                flag = false;
+                break;
+            }
+        (flag) ? cout << times << endl : cout << -1 << endl;
+    }
+}
