d64e0396fdf4f7061249fb24bff2b12ed4d7db56
Mo Cuishle <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2019-04-12 11:22:03 +0530

    Create beingGreedyForWater.cpp

---
+++ beingGreedyForWater.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, c;
+        cin >> n >> c;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++)
+            cin >> v[i];
+        sort(v.begin(), v.end());
+        int ans = 0, Count = 0;
+        for (int i = 0; i < n; i++) {
+            if (c - v[i] >= 0) {
+                c -= v[i];
+                Count++;
+            } else {
+                break;
+            }
+        }
+        cout << Count << endl;
+    }
+}
