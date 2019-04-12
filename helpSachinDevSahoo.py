28178449450eb63592fce369c00eb654a72a93b5
Mo Cuishle <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2019-04-12 12:32:02 +0530

    Create helpSachinDevSahoo.py

---
+++ helpSachinDevSahoo.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+
+t = int(input())
+while t:
+	t -= 1
+	ls, n = input().split()
+	n = int(n)
+	st = list(ls)
+	times = 0
+	for i in range(len(st) - n + 1):
+		if (st[i] == '0'):
+			for j in range(i, i+n):
+				if st[j] == '0':
+				    st[j] = '1'
+				else:
+				    st[j] = '0'
+			times += 1
+	flag = True
+	for i in range(len(st)):
+		if st[i] == '0':
+			flag = False
+			break
+	if flag:
+		print(times)
+	else:
+		print(-1)
