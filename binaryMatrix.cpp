88dedfce1176fd264c8b9127a5c26016365dd900
Mo Cuishle <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2019-04-12 15:25:18 +0530

    Create binaryMatrix.cpp

---
+++ binaryMatrix.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+
+int main(void) {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int n, m;
+    cin >> n >> m;
+    cin.ignore();
+    vector<pair<string, int>> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i].first;
+        v[i].second = i+1;
+    }
+    sort(v.begin(), v.end());
+    string str = v[n-1].first;
+    int i;
+    for (i = n-2; i >= 0; i--)
+        if (v[i].first != str)
+            break;
+    cout << v[i+1].second << endl;
+}
