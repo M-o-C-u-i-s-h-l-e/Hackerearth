cbcd576c5834be3ed4e26d18f5cb303b8901c0cc
Genie <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2019-09-25 00:44:17 +0530

    Create HPAndSplittingOfArray.cpp

---
+++ HPAndSplittingOfArray.cpp
@@ -0,0 +1,60 @@
+//    ____            _
+//   / ___| ___ _ __ (_) ___
+//  | |  _ / _ \ '_ \| |/ _ \
+//  | |_| |  __/ | | | |  __/
+//   \____|\___|_| |_|_|\___|
+
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define MAX 1000001
+
+vector<int> bit(MAX);
+
+int query(long idx) {
+    int res = 0;
+    while (idx) {
+        res += bit[idx];
+        idx -= (idx & -idx);
+    }
+    return res;
+}
+
+void update(long idx) {
+    while (idx < MAX) {
+        bit[idx]++;
+        idx += (idx & -idx);
+    }
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<long> v(n + 1);
+        for (int i = 1; i <= n; i++)
+            cin >> v[i];
+        v.push_back(v[1]);
+        v.erase(v.begin() + 1);
+        fill(bit.begin(), bit.end(), 0);
+        long ans = 0;
+        for (int i = n; i >= 1; i--) {
+            ans += query(v[i] - 1);
+            update(v[i]);
+        }
+        cout << ans << ' ';
+        for (int i = 1, add, sub; i < n; i++) {
+            sub = query(v[i] - 1);
+            add = n - query(v[i]);
+            ans += add - sub;
+            cout << ans << ' ';
+        }
+        cout << endl;
+    }
+}
