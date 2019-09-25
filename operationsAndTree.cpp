b87a31a815c69c0cc964dcaebf34caadda6a9026
Genie <49540191+M-o-C-u-i-s-h-l-e@users.noreply.github.com>
2019-09-26 01:34:20 +0530

    Update operationsAndTree.cpp

--- operationsAndTree.cpp
+++ operationsAndTree.cpp
@@ -11,32 +11,34 @@ using namespace std;
 #define endl '\n'
 #define MAX 100001
 
-int n, q, ts, v[MAX], stamp[MAX], ste[MAX];
+int n, q, ty, Time = 0, st[MAX], ed[MAX];
+long a, b;
+vector<bool> blocked(MAX, false);
 vector<int> adj[MAX];
 
-void dfs(int pos, int par) {
-	ts++;
-	stamp[pos] = ts;
-	for (int i : adj[pos])
+void dfs(int cur, int par) {
+	st[cur] = ++Time;
+	for (int i : adj[cur])
 		if (i != par)
-			dfs(i, pos);
-	ste[pos] = ts;
+			dfs(i, cur);
+	ed[cur] = Time;
 }
 
 struct node {
-	long l, r, lazy, sum;
+	int l, r;
+	long sum, lazy;
 };
 
 struct segTree {
 	node tree[MAX << 2];
 	
-	void pushUp(long k) {
+	void pushUp(int k) {
 		tree[k].sum = tree[k << 1].sum + tree[(k << 1) + 1].sum;
 	}
 	
-	void pushDown(long k) {
+	void pushDown(int k) {
 		if (tree[k].lazy) {
-			tree[k << 1].sum += (tree[k << 1].r - tree[k << 1].l + 1) * tree[k].lazy;
+			tree[k << 1].sum += (tree[k << 1].r - tree[k].l + 1) * tree[k].lazy;
 			tree[(k << 1) + 1].sum += (tree[(k << 1) + 1].r - tree[(k << 1) + 1].l + 1) * tree[k].lazy;
 			tree[k << 1].lazy += tree[k].lazy;
 			tree[(k << 1) + 1].lazy += tree[k].lazy;
@@ -44,26 +46,26 @@ struct segTree {
 		}
 	}
 	
-	void build(long l, long r, long k) {
+	void build(int l, int r, int k) {
 		tree[k].l = l, tree[k].r = r, tree[k].lazy = 0;
 		if (l == r) {
 			tree[k].sum = 0;
 			return;
 		}
-		long mid = (l + r) >> 1;
+		int mid = (l + r) >> 1;
 		build(l, mid, k << 1);
 		build(mid + 1, r, (k << 1) + 1);
 		pushUp(k);
 	}
 	
-	void update(long l, long r, long val, long k) {
+	void update(int l, int r, long val, int k) {
 		if (l <= tree[k].l && r >= tree[k].r) {
 			tree[k].sum += (tree[k].r - tree[k].l + 1) * val;
 			tree[k].lazy += val;
 			return;
 		}
 		pushDown(k);
-		long mid = (tree[k].l + tree[k].r) >> 1;
+		int mid = (tree[k].l + tree[k].r) >> 1;
 		if (l <= mid)
 			update(l, r, val, k << 1);
 		if (r > mid && l <= tree[k].r)
@@ -71,11 +73,11 @@ struct segTree {
 		pushUp(k);
 	}
 	
-	long query(long l, long r, long k) {
+	long query(int l, int r, int k) {
 		if (l <= tree[k].l && r >= tree[k].r)
 			return tree[k].sum;
 		pushDown(k);
-		long mid = (tree[k].l + tree[k].r) >> 1;
+		int mid = (tree[k].l + tree[k].r) >> 1;
 		long ans = 0;
 		if (l <= mid)
 			ans += query(l, r, k << 1);
@@ -89,56 +91,48 @@ int main(void) {
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 	cout.tie(0);
-
+	
 	cin >> n;
-	for (int i = 0, a, b; i < n - 1; i++) {
-		cin >> a >> b;
-		a--, b--;
-		adj[a].emplace_back(b);
-		adj[b].emplace_back(a);
+	for (int i = 0, x, y; i < n - 1; i++) {
+		cin >> x >> y;
+		adj[x].emplace_back(y);
+		adj[y].emplace_back(x);
 	}
-	for (int i = 0; i < n; i++)
-		cin >> v[i];
-	dfs(0, -1);
-    segTree temp, old;
-	old.build(1, n, 1);
+	dfs(1, 1);
+	segTree temp, old;
 	temp.build(1, n, 1);
-	for (int i = 0; i < n; i++)
-		old.update(stamp[i], stamp[i], v[i], 1);
+	old.build(1, n, 1);
+	for (int i = 1; i <= n; i++) {
+		cin >> a;
+		old.update(st[i], st[i], a, 1);
+	}
 	cin >> q;
-	set<int> bl;
 	while (q--) {
-		int ty, a, b;
 		cin >> ty;
 		if (ty == 1) {
 			cin >> a >> b;
-			a--;
-			temp.update(stamp[a], ste[a], b, 1);
+			temp.update(st[a], ed[a], b, 1);
 		} else if (ty == 2) {
 			cin >> a >> b;
-			a--;
-			temp.update(stamp[a], stamp[a], b, 1);
+			temp.update(st[a], st[a], b, 1);
 		} else if (ty == 3) {
 			cin >> a;
-			a--;
-			if (bl.count(a)) {
-				temp.update(stamp[a], stamp[a], -temp.query(stamp[a], stamp[a], 1), 1);
-				bl.erase(a);
+			if (blocked[a]) {
+				temp.update(st[a], st[a], -temp.query(st[a], st[a], 1), 1);
 			} else {
-				old.update(stamp[a], stamp[a], temp.query(stamp[a], stamp[a], 1), 1);
-				temp.update(stamp[a], stamp[a], -temp.query(stamp[a], stamp[a], 1), 1);
-				bl.insert(a);
+				old.update(st[a], st[a], temp.query(st[a], st[a], 1), 1);
+				temp.update(st[a], st[a], -temp.query(st[a], st[a], 1), 1);
 			}
+			blocked[a] = blocked[a] ? false : true;
 		} else {
 			cin >> a;
-			a--;
-			if (bl.count(a)) {
-				temp.update(stamp[a], stamp[a], -temp.query(stamp[a], stamp[a], 1), 1);
+			if (blocked[a]) {
+				temp.update(st[a], st[a], -temp.query(st[a], st[a], 1), 1);
 			} else {
-				old.update(stamp[a], stamp[a], temp.query(stamp[a], stamp[a], 1), 1);
-				temp.update(stamp[a], stamp[a], -temp.query(stamp[a], stamp[a], 1), 1);
+				old.update(st[a], st[a], temp.query(st[a], st[a], 1), 1);
+				temp.update(st[a], st[a], -temp.query(st[a], st[a], 1), 1);
 			}
-			cout << old.query(stamp[a], stamp[a], 1) << endl;
+			cout << old.query(st[a], st[a], 1) << endl;
 		}
 	}
 }
